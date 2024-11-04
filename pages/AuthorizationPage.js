73aa92b8fe2f92535288b590a768ff1b24109753
KKopilka <yana.stepanckova@yandex.ru>
2024-11-04 19:35:20 +0300

    Add negative tests for username, password and email

--- pages/AuthorizationPage.js
+++ pages/AuthorizationPage.js
@@ -14,10 +14,11 @@ exports.Authorization = class Authorization {
     this.btnAcademy = page.locator('#app-header a').filter({ hasText: 'Академия' });
     this.btnP2P = page.locator('#app-header a').filter({ hasText: 'P2P' });
     this.btnInternationalExchange = page.locator('#app-header a').filter({ hasText: 'Международный обмен' });
-    this.inpUsername = page.locator('#input-3198');
+    // this.inpUsername = page.locator('#input-1728');
+    this.inpUsername = page.locator('(//div[contains(@class, "v-text-field__slot")]//input)[1]');
     this.inpEmail = page.locator('#username');
-    this.inpPassword = pahe.locator('#new-password');
-    this.inpReferralCode = page.locator('#input-3238');
+    this.inpPassword = page.locator('#new-password');
+    this.inpReferralCode = page.locator('input[specialtoken="k-text-field-primary"][type="text"]').first();
     this.chkbxAgreement = page.locator('#input-3250');
     this.btnNext = page.getByRole('button', { name: 'Далее' });
     this.btnOpenPassword = page.getByLabel('preview open');
@@ -28,7 +29,98 @@ exports.Authorization = class Authorization {
     this.btnLoginApple = page.getByTestId('apple');
   }
 
-  async emptyInputFields() {
+  async emptyInput() {
     await this.btnNext.click();
+    // Получаем все элементы с сообщением "Поле не заполнено"
+    const errorMessages = this.page.locator('span.k-text', { hasText: 'Поле не заполнено' });
+    
+    // Ожидаем, что таких сообщений будет ровно три
+    await expect(errorMessages).toHaveCount(3);
+
+    // Дополнительно проверяем, что текст каждого сообщения - "Поле не заполнено"
+    for (let i = 0; i < 3; i++) {
+      await expect(errorMessages.nth(i)).toHaveText('Поле не заполнено');
+      console.log(`Ошибка ${i + 1}: Поле не заполнено - найдено на странице.`);
+    }
+  }
+
+  async shortUsername(errorMessage = 'Имя пользователя недействительно') {
+    await this.inpUsername.fill('abc');
+    await this.btnNext.click();
+    await this.page.waitForTimeout(3000);
+    const error = this.page.locator('.v-card > .css-grid');
+    await expect(error).toContainText(errorMessage);
+  }
+
+  async longUsername(errorMessage = 'Имя пользователя недействительно') {
+    await this.inpUsername.fill('a'.repeat(33));
+    await this.btnNext.click();
+    await this.page.waitForTimeout(3000);
+    const error = this.page.locator('.v-card > .css-grid');
+    await expect(error).toContainText(errorMessage);
+  }
+
+  async startsInvalidCharacterUsername(errorMessage = 'Имя пользователя недействительно') {
+    await this.inpUsername.fill('1username');
+    await this.btnNext.click();
+    await this.page.waitForTimeout(3000);
+    const error = this.page.locator('.v-card > .css-grid');
+    await expect(error).toContainText(errorMessage);
+  }
+
+  async containsInvalidCharacterUsername() {
+    await this.inpUsername.fill('user!name');
+    const errorMessage = this.page.locator('span.k-text', { hasText: 'Введены недопустимые символы: !' });
+    await expect(errorMessage).toBeVisible();
+  }
+
+  async incorrectEmailFormat() {
+    await this.inpEmail.fill('userexample.com');
+    await this.btnNext.click();
+    await expect(page.locator('span.k-text', { hasText: 'Формат e-mail: username@test.ru' })).toBeVisible();
+
+    await this.inpEmail.fill('user@wrongdomain');
+    await this.btnNext.click();
+    await expect(page.locator('span.k-text', { hasText: 'Формат e-mail: username@test.ru' })).toBeVisible();
+  }
+
+  async shortPassword() {
+    await this.inpPassword.fill('abcabc');
+    await this.btnNext.click();
+    await expect(page.locator('span.k-text', { 
+      hasText: 'Пароль должен содержать минимум 8 символов' 
+    })).toBeVisible();
+  }
+
+  async longPassword() {
+    await this.inpPassword.fill('a'.repeat(65));
+    await this.btnNext.click();
+    await expect(page.locator('span.k-text', { 
+      hasText: 'Превышен лимит символов: 64 максимум' 
+    })).toBeVisible();  
+  }
+
+  async lowercasePassword() {
+    await this.inpPassword.fill('abba2233445566');
+    await this.btnNext.click();
+    await expect(page.locator('span.k-text', { 
+      hasText: 'Пароль должен содержать от 8 до 64 символов, включая заглавные буквы и цифры' 
+    })).toBeVisible();  
+  }
+
+  async uppercasePassword() {
+    await this.inpPassword.fill('ABBA2233445566');
+    await this.btnNext.click();
+    await expect(page.locator('span.k-text', { 
+      hasText: 'Пароль должен содержать от 8 до 64 символов, включая заглавные буквы и цифры' 
+    })).toBeVisible();  
+  }
+
+  async incorrectReferralCodeFormat() {
+    await this.inpReferralCode.fill('---');
+    console.log(this.btnNext.isDisabled());
+    // await expect(page.locator('span.k-text', { 
+    //   hasText: 'Пароль должен содержать от 8 до 64 символов, включая заглавные буквы и цифры' 
+    // })).toBeVisible();  
   }
 }
---
+++ tests/functional/authTests.spec.js
@@ -0,0 +1,56 @@
+const { test, expect } = require('@playwright/test');
+const { Authorization } = require('../../pages/AuthorizationPage.js');
+
+let authPage;
+
+test.beforeEach(async ({ page }) => {
+  authPage = new Authorization(page);
+  await page.goto('https://koshelek.ru/authorization/signup');
+});
+
+test.describe('Негативные тесты для страницы регистрации', () => {
+  test('1. Пыстые поля', async () => {
+      await authPage.emptyInput();
+  });
+
+  test('2. Имя пользователя короче 6 символов', async () => {
+    await authPage.shortUsername();
+  });
+
+  test('3. Имя пользователя длиннее 32 символов', async () => {
+    await authPage.longUsername();
+  });
+
+  test('4. Имя пользователя начинается с недопустимого символа', async () => {
+    await authPage.startsInvalidCharacterUsername();
+  });
+
+  test('5. Имя пользователя содержит недопустимые символы', async () => {
+    await authPage.containsInvalidCharacterUsername();
+  });
+
+  test('6. Некорректный формат Email', async () => {
+      await authPage.incorrectEmailFormat();
+  });
+
+  test('7. Ввод пароля короче 8 символов', async () => {
+    await authPage.shortPassword();
+  });
+
+  test('8. Ввод пароля длиннее 64 символов', async () => {
+    await authPage.longPassword();
+  });
+
+  test('9. Ввод пароля без заглавных букв', async () => {
+    await authPage.lowercasePassword();
+  });
+
+  test('10. Ввод пароля только с заглавными буквами', async () => {
+    await authPage.uppercasePassword();
+  });
+
+  test('10. ', async () => {
+    await authPage.incorrectReferralCodeFormat();
+  });
+  
+});
